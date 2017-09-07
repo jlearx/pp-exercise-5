e94e6bd4fb51a48474e522a7a5ce91e1dec004de
U-AMR\jlearx <jlearx@jlearx-MOBL1.amr.corp.intel.com>
2017-09-07 15:06:31 -0700

    Implemented one line solution

--- src/main.py
+++ src/main.py
@@ -5,4 +5,14 @@ Created on Sep 7, 2017
 '''
 
 if __name__ == '__main__':
-    pass
+    a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+    b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+    c = []
+    
+    # One line solution
+    
+    [c.append(e) for e in a+b if ((e in a) and (e in b) and (c.count(e) == 0))]
+
+    print(c)
+    
+    
